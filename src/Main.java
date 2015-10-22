e830f56268be74bbf3901e02fe7d8e0bad6edc49
Silvan Adrian <hallo@silvanadrian.ch>
2015-10-22 18:16:10 +0200

    pom.xml

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-package main.java;
+
 
 import org.libvirt.*;
 public class Main {
--- test/TestMain.java
+++ test/TestMain.java
@@ -1,14 +1,18 @@
-package test.java;
+
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import main.java.Main;
-
 public class TestMain {
 	@Test
 	public void testGetString() {
 		assertEquals(Main.getString(), "Hallo Welt");
+	}
+	
+	
+	@Test
+	public void testString() {
+		assertEquals("Hallo Welt" , "Hallo Welt");
 	}
 }
