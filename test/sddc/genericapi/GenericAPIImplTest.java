98d591286fb7a038d09b238d94ec66b2c1095a24
Fabian Binna <fbinna@hsr.ch>
2015-10-29 12:01:10 +0100

    Debug Storage Test

--- test/sddc/genericapi/GenericAPIImplTest.java
+++ test/sddc/genericapi/GenericAPIImplTest.java
@@ -11,15 +11,11 @@ import sddc.dataaccess.IGenericAPIFacade;
 public class GenericAPIImplTest {
 
 	private IGenericAPIFacade api;
-	private String storageConfig = "<pool type=\"disk\"><name>vdb2</name><source><device path='/dev/vdb2'/></source><target><path>/dev</path></target></pool>";
-	private String storageUuid;
-
+	
 	@Before
 	public void setUp() throws Exception {
 		api = new GenericAPILibVirt();
 		api.connect("test:///default", false);
-		
-		storageUuid = api.createStorage(storageConfig);
 	}
 
 	@After
@@ -28,20 +24,12 @@ public class GenericAPIImplTest {
 	}
 	
 	@Test
-	public void testCreateStorage() throws LibvirtException {
-		String uuid = api.createStorage(storageConfig);
-		Assert.assertNotNull(uuid);
-		Assert.assertNotNull(api.getStorage(uuid));
-	}
-	
-	@Test
-	public void testDeleteStorage() throws LibvirtException {
-		api.deleteStorage(storageUuid);
-		Assert.assertNull(api.getStorage(storageUuid));
-	}
-	
-	@Test
-	public void testGetStorage() throws LibvirtException {
+	public void testStorage() throws LibvirtException {
+		String storageConfig = "<pool type=\"disk\"><name>vdb2</name><source><device path='/dev/vdb2'/></source><target><path>/dev</path></target></pool>";
+		String storageUuid;
+		
+		storageUuid = api.createStorage(storageConfig);
+		Assert.assertNotNull(storageUuid);
 		Assert.assertNotNull(api.getStorage(storageUuid));
 		api.deleteStorage(storageUuid);
 		Assert.assertNull(api.getStorage(storageUuid));
