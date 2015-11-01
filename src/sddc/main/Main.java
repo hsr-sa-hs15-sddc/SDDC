ec4ef70095d992add7b826171799baaa1c3510b7
Silvan Adrian <hallo@silvanadrian.ch>
2015-11-01 10:31:03 +0100

    Very Easy Main Class + Assambly Plugin to compile a JAR File

--- src/sddc/main/Main.java
+++ src/sddc/main/Main.java
@@ -1,12 +1,31 @@
 package sddc.main;
 
 
+import java.io.Console;
+import java.io.File;
+
 import org.libvirt.*;
+
+import sddc.dataaccess.IGenericAPIFacade;
+import sddc.genericapi.GenericAPILibVirt;
 public class Main {
+	
+private static String connection,filename;
+private static IGenericAPIFacade api;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		 Console console = System.console();
+	     connection = console.readLine("Please enter Connection String : ");
+	     api = new GenericAPILibVirt();
+	     try {
+			api.connect(connection, false);
+		} catch (LibvirtException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	     filename = console.readLine("Please enter Name of File : ");
+	     File file = new File(filename);
+	     //createMethod(file.toString(););
 	}
 	
 }
