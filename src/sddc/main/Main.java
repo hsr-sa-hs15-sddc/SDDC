3567ce2fc5883065b9b1fa9719304fe6d7be96e7
Silvan Adrian <hallo@silvanadrian.ch>
2015-11-01 16:11:06 +0100

    Changed DefaultCharset (Findbugs Issue)

--- src/sddc/main/Main.java
+++ src/sddc/main/Main.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -53,7 +54,7 @@ public class Main {
 		 
 		 
 		 IPersistenceFacade persistence = new PersistenceFake();
-		 System.out.println("testservice id = " + persistence.storeService(readFile("testservice.xml", Charset.defaultCharset())));
+		 System.out.println("testservice id = " + persistence.storeService(readFile("testservice.xml", StandardCharsets.UTF_8)));
 		 
 		 service = new CRUDService(api, persistence);
 		 
