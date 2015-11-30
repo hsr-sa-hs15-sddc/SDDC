9b0e9ece58b138d8fbd823837a0f39fb12845515
Fabian Binna <fbinna@hsr.ch>
2015-11-30 17:39:05 +0100

    Dependency Injection + LibVirtServiceModuleHandlerTest

---
+++ src/test/java/sddc/genericapi/LibVirtServiceModuleHandlerTest.java
@@ -0,0 +1,49 @@
+package sddc.genericapi;
+
+import static org.junit.Assert.*;
+
+import java.nio.charset.Charset;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import junit.framework.Assert;
+import sddc.services.domain.Category;
+import sddc.services.domain.Identifier;
+import sddc.services.domain.Provider;
+import sddc.services.domain.ServiceModule;
+import sddc.services.domain.Size;
+import sddc.util.FileUtil;
+
+public class LibVirtServiceModuleHandlerTest {
+	
+	private IServiceModuleHandler handler;
+
+	@Before
+	public void setUp() throws Exception {
+		Resource resource = new ClassPathResource("applicationContext.xml");
+		
+		@SuppressWarnings("deprecation")
+		BeanFactory factory = new XmlBeanFactory(resource);
+		
+		handler = (IServiceModuleHandler) factory.getBean("LibVirtServiceModuleHandler");
+	}
+
+	@Test
+	public void createTest() {
+		
+		String config = FileUtil.getContentOfFile("src/test/resources/LibVirtStorageConfigExample.xml", 
+				Charset.defaultCharset(), false);
+		
+		ServiceModule module = new ServiceModule("StorageConfigTest", Size.L, Provider.LibVirt, Category.Storage, config);
+				
+		Identifier identifier = handler.create(module);
+		
+		Assert.assertNull(identifier);
+	}
+
+}
